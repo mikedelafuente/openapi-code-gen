cb0a13e8c936f1a5bfb22645140cb3298aeea058
Weekend Project <weekendprojectapp@gmail.com>
2021-05-13 22:11:19 -0700

    Initial project setup. Iterating through parts of the openAPI spec works.

---
+++ src/CodeGen/CodeGen.Core/OpenApiHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Readers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeGen.Core
+{
+    public static class Test
+    {
+        public static string DoIt()
+        {
+            return "Hello";
+        }
+    }
+
+    public class OpenApiHelper
+    {
+        public OpenApiHelper(string openApiFilePath)
+        {
+            OpenApiDiagnostic openApiDiag;
+
+            var openApiJson = System.IO.File.ReadAllText(openApiFilePath);
+
+            // Read V3 as YAML
+            this.ApiDoc = new Microsoft.OpenApi.Readers.OpenApiStringReader().Read(openApiJson, out openApiDiag);  //new OpenApiStreamReader().Read(stream, out var diagnostic);
+        }
+
+        public OpenApiDocument ApiDoc { get; }
+
+        public List<Models.Tag> GetTagNames()
+        {
+            return ApiDoc.Tags.Select(x => new Models.Tag() { OriginalName = x.Name, DisplayName = Regex.Replace(x.Name, "[^a-zA-Z0-9]+", string.Empty) }).ToList();
+        }
+    }
+}
